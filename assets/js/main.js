5835363e494a1532773277a21f3eb534b971d9d7
MartinaDR1 <derosemartina933@gmail.com>
2023-03-07 15:24:48 +0100

    code changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 // Chiedere all'utente il suo nome
-let userName = prompt('Inserisci il tuo nome')
+let userName = prompt('Inserisci il tuo nome');
 
 // Stampo in console risposta dell'utente
 console.log(userName);
 
 // Chiedere all'utente il suo cognome
-let userSurname = prompt('Inserisci il tuo cognome')
+let userSurname = prompt('Inserisci il tuo cognome');
 
 // Stampo in console risposta dell'utente
 console.log(userSurname);
 
 // Chiedere all'utente il suo colore preferito
-let color = prompt('Inserisci il tuo colore preferito')
+let color = prompt('Inserisci il tuo colore preferito');
 
 // Stampo in console risposta dell'utente
 console.log(color);
 
 // Inserisco nell'HTML il risultato totale
-let userComplete = document.getElementById('user')
+let userComplete = document.getElementById('user');
 
-userComplete.innerHTML = `${userName}${userSurname}${color}${23}`
+userComplete.innerHTML = `${userName}${userSurname}${color}${23}`;
