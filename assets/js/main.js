4f28f478d437b538e6567c109be2876d6d4a54ed
MartinaDR1 <derosemartina933@gmail.com>
2023-03-07 16:34:32 +0100

    add const

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,18 @@ let userName = prompt('Inserisci il tuo nome');
 console.log(userName);
 
 // Chiedere all'utente il suo cognome
-let userSurname = prompt('Inserisci il tuo cognome');
+const userSurname = prompt('Inserisci il tuo cognome');
 
 // Stampo in console risposta dell'utente
 console.log(userSurname);
 
 // Chiedere all'utente il suo colore preferito
-let color = prompt('Inserisci il tuo colore preferito');
+const color = prompt('Inserisci il tuo colore preferito');
 
 // Stampo in console risposta dell'utente
 console.log(color);
 
 // Inserisco nell'HTML il risultato totale
-let userComplete = document.getElementById('user');
+const userComplete = document.getElementById('user');
 
 userComplete.innerHTML = `${userName}${userSurname}${color}${23}`;
